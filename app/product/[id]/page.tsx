da4fa8ce8e95d00f7f4cb9192bfef26769a7a819
Julie Proteau <julie.proteau@hotmail.com>
2026-01-18 11:23:43 -0500

    keepImgProportions

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -3,10 +3,20 @@
 import { products } from '../../data/products';
 import { useParams } from 'next/navigation';
 import ProductCard from '../../products/ProductCard';
+import { Product } from '@/lib/db/types';
+import { useState } from 'react';
+import { useCart } from '@/app/contexts/CartContext';
 
 export default function ProductPage() {
   const params = useParams(); // id du produit depuis l'URL
   const product = products.find(p => p.id === params.id);
+  const [added, setAdded] = useState(false);
+  const { addToCart } = useCart();
+
+  const handleAddToCart = (product: Product) => {
+    addToCart(product);
+    setAdded(true);
+  };
 
   if (!product) return <p className="text-center py-8">Produit introuvable</p>;
 
@@ -17,7 +27,8 @@ export default function ProductPage() {
           product={product}
           expanded={true}
           useFullImg={true}
-          showAddToCart={true}
+          onAddToCart={handleAddToCart} // la fonction qui ajoute au panier
+          added={added}                 // true si déjà ajouté
         />
       </div>
     </div>
