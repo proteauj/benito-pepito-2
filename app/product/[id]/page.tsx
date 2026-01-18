9cb4af157bed79c094b9b5336cd80834452feed9
Julie Proteau <julie.proteau@hotmail.com>
2026-01-18 10:48:10 -0500

    Try fix look mobile

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -11,7 +11,7 @@ export default function ProductPage() {
   if (!product) return <p className="text-center py-8">Produit introuvable</p>;
 
   return (
-    <div className="container mx-auto px-4 py-8 bg-white">
+    <div className="container mx-auto px-4 py-8">
       <ProductCard
         product={product}
         expanded={true}      // affiche toutes les infos
