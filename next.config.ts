901cc0297cf9c6085d491df200715d30b1faf281
Julie Proteau <julie.proteau@hotmail.com>
2026-01-21 16:39:39 -0500

    Fix fetch products

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  turbopack: {},
   images: {
     unoptimized: true,
     disableStaticImages: true,
@@ -11,25 +9,6 @@ const nextConfig: NextConfig = {
   experimental: {
     disableOptimizedLoading: true,
   },
-  webpack: (config) => {
-    // Désactiver le loader d'image de Next.js
-    config.module.rules = config.module.rules.filter(
-      (rule: { loader: string | string[] }) =>
-        !rule.loader?.includes("next-image-loader")
-    );
-
-    // Alias pour faciliter les imports
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./app"),
-      "@/components": path.resolve(__dirname, "./app/components"),
-      "@/data": path.resolve(__dirname, "./app/data"),
-      "@/i18n": path.resolve(__dirname, "./app/i18n"),
-    };
-
-    return config;
-  },
-
   // Ajouté pour forcer Next.js à prendre le bon root et éviter l'erreur Prisma
   outputFileTracingRoot: __dirname,
 };
