9bf243c6ecbc7a5c666a6d9336f8b1006950cb48
Julie Proteau <julie.proteau@hotmail.com>
2026-01-21 19:18:51 -0500

    Align img left

--- app/products/ProductCard.tsx
+++ app/products/ProductCard.tsx
@@ -40,7 +40,7 @@ export default function ProductCard({
       >
         <div
           className={`
-            w-36 overflow-hidden mx-auto
+            w-100 overflow-hidden mx-auto
             ${!useFullImg && !keepImgProportions ? 'aspect-square' : ''}
           `}
         >
