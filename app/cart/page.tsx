8a4aaa2d3490206cd61d1512d45ff472aa9808e2
Julie Proteau <julie.proteau@hotmail.com>
2026-01-23 13:27:26 -0500

    Improve form style

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -216,7 +216,7 @@ export default function CartPage() {
             ))}
             <br/>
             <div className="flex-1 rounded shadow bg-white p-4">
-              <form className="grid grid-cols-1 gap-1 bg-white p-6">
+              <form className="grid grid-cols-1 gap-2 bg-white p-6">
                   <label className="block text-sm font-semibold text-black mb-1">{t('contact.form.email')}</label>
                   <input
                     type="email"
