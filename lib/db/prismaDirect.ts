cbebc9c53cdf576484e7755e3c0810ee0ff123da
Julie Proteau <julie.proteau@hotmail.com>
2026-01-26 14:51:18 -0500

    Chg to DB_DATABASE_URL

--- lib/db/prismaDirect.ts
+++ lib/db/prismaDirect.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 export const prismaDirect = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL, // connexion directe
+      url: process.env.DB_DATABASE_URL, // connexion directe
     },
   },
 });
